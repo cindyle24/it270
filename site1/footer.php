f2cd30a9b5bb6feeef80ed8fe4848dacf4235742
cindyle24 <45842097+cindyle24@users.noreply.github.com>
2021-07-12 22:51:23 -0700

    Add files via upload

---
+++ site1/footer.php
@@ -0,0 +1,15 @@
+<footer>
+    <div class="copyright">
+<ul>
+    <li>Copyright <?php echo date('y'); ?></li>
+    <li>All Rights Reserved </li>
+    <li><a href="">Terms of use</li>
+    <li><a href="cbleonline.com/it270">Web Design by Cindy</li>
+
+
+</ul>
+</div>
+</footer>
+</body>
+
+</html>
